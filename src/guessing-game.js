4d7603ba3c62c90ea3bbbab471546cd3f182876f
Your Name <you@example.com>
2022-08-10 00:15:35 +0300

    feat: add task functionality

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+    }
+  
     setRange(min, max) {
-
+        this.minvalue = min;
+        this .maxvalue = max;
+        this.guess()
     }
-
+  
     guess() {
-
+        
+        let number = Math.round((this.maxvalue+this.minvalue)/2)
+        return number
     }
-
+  
     lower() {
-
+        this.setRange(this.minvalue, this.guess())
     }
-
+  
     greater() {
-
+        this.setRange( this.guess(), this.maxvalue)
     }
 }
 
